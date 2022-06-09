44fff5b912580db1c4564aac89c35cbc91c2a151
Eric Wang <tw3ad@virginia.edu>
2022-06-09 19:57:32 -0400

    EVERYTING IS WORKING, GOT THE ROOMS TO DISPLAY!

--- finder/models.py
+++ finder/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    room_name = models.CharField(max_length=200)
+    visible = models.IntegerField(default=1)
+    def __str__(self):
+        return self.room_name
--- finder/views.py
+++ finder/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # from .forms import NameForm
+from .models import Room
+
+
+# def index(request):
+#     return render(request, 'finder/index.html') 
 
 def index(request):
-    return render(request, 'finder/index.html')
+    rooms = [ "Rice Hall " + str(i) for i in range(120, 140) ]
+    return render(request, 'finder/index.html', {'rooms': rooms})
 
     
