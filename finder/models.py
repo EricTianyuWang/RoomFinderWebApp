a73973978624804d74deeac974c5498c0cf7dbcc
Eric Wang <tw3ad@virginia.edu>
2022-06-13 17:33:16 -0400

    6-13

--- finder/models.py
+++ finder/models.py
@@ -1,7 +1,32 @@
+from re import template
 from django.db import models
+from django.http import HttpResponse
 
-class Room(models.Model):
-    room_name = models.CharField(max_length=200)
-    visible = models.IntegerField(default=1)
+
+class BUILDING(models.Model):
+    BUILDING_NAME = models.CharField(max_length=255)
+    BUILDING_CITY = models.CharField(max_length=255)
+    BUILDING_ZIP = models.CharField(max_length=255)
+    BUILDING_STREET = models.CharField(max_length=255)
+    BUILDING_STATE = models.CharField(max_length=255)
+
+class ROOM(models.Model):
+    BUILDING_ID = models.ForeignKey(BUILDING, on_delete=models.CASCADE)
+    ROOM_NUMBER =  models.CharField(max_length=255)
+    CAPACITY = models.IntegerField()
+
+class STUDENT(models.Model):
+    STUDENT_LNAME = models.CharField(max_length=255)
+    STUDENT_FNAME = models.CharField(max_length=255)
+    STUDENT_LNAME = models.CharField(max_length=255)
+    STUDENT_EMAIL = models.CharField(max_length=255)
     def __str__(self):
-        return self.room_name
+        return self.STUDENT_FNAME
+
+class STUDENT_ROOM(models.Model):
+    STUDENT_ID = models.ForeignKey(STUDENT, on_delete=models.CASCADE)
+    ROOM_ID = models.ForeignKey(ROOM, on_delete=models.CASCADE)
+
+
+
+
--- finder/views.py
+++ finder/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 # from .forms import NameForm
-from .models import Room
-
+from .models import STUDENT, ROOM, STUDENT_ROOM
+from django.template import loader
+from django.http import HttpResponse
 
 # def index(request):
 #     return render(request, 'finder/index.html') 
@@ -10,4 +11,9 @@ def index(request):
     rooms = [ "Rice Hall " + str(i) for i in range(120, 140) ]
     return render(request, 'finder/index.html', {'rooms': rooms})
 
+def testing(request):
+    my_data = STUDENT.objects.all()
+    template = loader.get_template('index.html')
+    context = { 'students': my_data }
+    return HttpResponse(template.render(context, request))
     
