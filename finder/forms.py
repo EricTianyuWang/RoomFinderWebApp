7c6059cea82da68dd3066140eda6a576e4a5ac3a
Eric Wang <tw3ad@virginia.edu>
2022-06-15 17:37:32 -0400

    able to search times

--- finder/forms.py
+++ finder/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+#from .models import Dweet
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class DweetForm(forms.Form):
+    body = forms.CharField(label='building_time', required=True)
--- finder/views.py
+++ finder/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
-# from .forms import NameForm
-from .models import Student
+from .forms import DweetForm
+from .models import Student, Room
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # def index(request):
 #     return render(request, 'finder/index.html') 
 
 def index(request):
-    students = Student.objects.all()
-    context = { 'students': students }
+    user_clicked_search = False
+    data = ""
+    time_searched = ""
+    if request.method == "POST":
+        #user_clicked_search = True
+        time_searched = request.POST["time_searched"]
+        data = Room.objects.raw(
+            f"""
+            SELECT 
+                TIME_BLOCK.TIME_ID,
+                TIME_BLOCK.START_TIME,
+                AVAILROOM.AVAILABILITY_ID,
+                AVAILROOM.ROOM_ID,
+                AVAILROOM.ROOM_NUMBER
+            FROM
+                TIME_BLOCK
+            INNER JOIN
+                (SELECT 
+                    AVAILABILITY.TIME_ID,
+                    AVAILABILITY.AVAILABILITY_ID,
+                    ROOM.ROOM_ID,
+                    ROOM.ROOM_NUMBER
+                FROM
+                    AVAILABILITY
+                INNER JOIN ROOM
+                WHERE AVAILABILITY.ROOM_ID = ROOM.ROOM_ID) AS AVAILROOM ON TIME_BLOCK.TIME_ID = AVAILROOM.TIME_ID
+            WHERE TIME_BLOCK.TIME_ID = '{time_searched}';
+            """
+        )
+
+    context = { 'user_clicked_search': user_clicked_search, 'data': data, 'time_searched': time_searched }
     return render(request, 'finder/index.html', context)
