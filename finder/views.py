e44765ff84fa73f309d89f44fbf193fa7cfad322
sna4wu <106699090+sna4wu@users.noreply.github.com>
2022-06-16 18:08:06 -0400

    omgggggggggggg

--- finder/views.py
+++ finder/views.py
@@ -9,10 +9,12 @@ def index(request):
     user_clicked_search = False
     rooms_found = ""
     time_searched = ""
+    day_searched = ""
     reservation_room = ""
     reservation_time = ""
-    if "time_searched" in request.POST:
+    if "time_searched" in request.POST and "day_searched" in request.POST:
         user_clicked_search = True
+        day_searched = request.POST["day_searched"]
         time_searched = request.POST["time_searched"]
         rooms_found = Room.objects.raw(
             f"""
@@ -21,24 +23,27 @@ def index(request):
                 TIME_BLOCK.START_TIME,
                 AVAILROOM.AVAILABILITY_ID,
                 AVAILROOM.ROOM_ID,
-                AVAILROOM.ROOM_NUMBER
+                AVAILROOM.ROOM_NUMBER,
+                AVAILROOM.AVAILABLE_DAY
             FROM
                 TIME_BLOCK
             INNER JOIN
                 (SELECT 
                     AVAILABILITY.TIME_ID,
                     AVAILABILITY.AVAILABILITY_ID,
+                    AVAILABILITY.AVAILABLE_DAY,
                     ROOM.ROOM_ID,
                     ROOM.ROOM_NUMBER
                 FROM
                     AVAILABILITY
                 INNER JOIN ROOM
                 WHERE AVAILABILITY.ROOM_ID = ROOM.ROOM_ID) AS AVAILROOM ON TIME_BLOCK.TIME_ID = AVAILROOM.TIME_ID
-            WHERE TIME_BLOCK.TIME_ID = '{time_searched}';
+            WHERE TIME_BLOCK.START_TIME = '{time_searched}' AND AVAILROOM.AVAILABLE_DAY = '{day_searched}';
             """
         )
-
-    if "reservation_room" in request.POST and "reservation_time" in request.POST and "student_email" in request.POST:
+    reserved_day = ""
+    if "reservation_room" in request.POST and "reservation_time" in request.POST and "student_email" in request.POST and "reserved_day" in request.POST:
+        reserved_day = request.POST["reserved_day"]
         reservation_room =  request.POST["reservation_room"]
         reservation_time =  request.POST["reservation_time"]
         student_email = request.POST["student_email"]
@@ -53,25 +58,27 @@ def index(request):
         for a in rooms:
             room_id = a.room_id
 
-        curDT = datetime.now()
-        reservation_date = curDT.strftime("%m/%d/%Y")
+        #curDT = datetime.now()
+        #reservation_date = curDT.strftime("%m/%d/%Y")
 
         times = TimeBlock.objects.raw(f"SELECT TIME_ID FROM TIME_BLOCK WHERE TIME_BLOCK.START_TIME LIKE '{reservation_time}'")
         my_time_id = ""
         for b in times:
             my_time_id = b.time_id
-
+        print("---------------------", my_time_id)
         with connection.cursor() as cursor:
             # the format looks like this: INSERT INTO RESERVATION VALUES(RESERVATION_ID, STUDENT_ID, ROOM_ID, RESERVATION_DATE, TIME_ID)
             #cursor.execute(f"INSERT INTO RESERVATION VALUES(RESERVATION_ID, {student_id}, {room_id}, '{reservation_date}', {time_id})")
-            cursor.execute(f"INSERT INTO RESERVATION VALUES(RESERVATION_ID, {student_id}, {room_id}, '{reservation_date}', {my_time_id})")
+            cursor.execute(f"INSERT INTO RESERVATION VALUES(RESERVATION_ID, {student_id}, {room_id}, '{reserved_day}', {my_time_id})")
+            cursor.execute(f"DELETE FROM AVAILABILITY WHERE AVAILABILITY.ROOM_ID = {room_id} AND AVAILABILITY.TIME_ID = {my_time_id}")
             
     context = {
         'user_clicked_search': user_clicked_search,
         'rooms_found': rooms_found, 
         'time_searched': time_searched,
         'reservation_room': reservation_room,
-        'reservation_time': reservation_time
+        'reservation_time': reservation_time,
+        'reserved_day': reserved_day
     }
     return render(request, 'finder/index.html', context)
 
